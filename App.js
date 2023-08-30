aaeb2e4d9b463ab65f0f49b97c6dd1f5619fe7b8
IvashkaVIP <ivashka.co@gmail.com>
2023-08-30 20:07:34 +0300

    registration page

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import SVGImg from "./assets/images/add.svg";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground
+        source={require("./assets/images/PhotoBG.jpg")}
+        style={styles.image}
+      >
+        <View style={styles.content}>
+          <View style={styles.avatar}>
+            {/* <SVGImg width={25} height={25} /> */}
+          </View>
+          <Text style={styles.text}>Реєстрація</Text>
+        </View>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,55 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+  },
+  avatar: {
+    width: 120,
+    height: 120,
+    backgroundColor: "#f6f6f6",
+    borderRadius: 16,
+    borderBlockColor: "black",
+    borderWidth: 1,
+    // position: "relative",
+    // bottom: 60,
+
+    marginTop: -60,
+    marginBottom: 32,
+
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.75,
+    shadowRadius: 4,
+    elevation: 4,
+  },
+  add: {
+    width: 25,
+    height: 25,
+
+    position: "relative",
+    bottom: 16,
+    left: 12,
+  },
+  content: {
+    flex: 1,
+    backgroundColor: "white",
+    marginTop: 263,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+
+    alignItems: "center",
+    // justifyContent: "center",
+  },
+  text: {
+    color: "#212121",
+    fontSize: 30,
+    fontWeight: "medium",
   },
 });
