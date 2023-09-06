1f7fdc29a3e143e03654a6e1c6d0317cc11144e9
IvashkaVIP <ivashka.co@gmail.com>
2023-09-06 23:45:43 +0300

    add PostsScreen

--- App.js
+++ App.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { useState } from "react";
 import { useFonts } from "expo-font";
-import {StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 import LoginScreen from "./screens/auth/LoginScreen";
 import RegistrationScreen from "./screens/auth/RegistrationScreen";
+import PostsScreen from "./screens/auth/PostsScreen";
 
 export default function App() {
-   const [fontsLoaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
     "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
   });
-   if (!fontsLoaded) {
-     return null;
-   }
+  if (!fontsLoaded) {
+    return null;
+  }
 
-      
   return (
-    <LoginScreen />   
+    <LoginScreen />
     // <RegistrationScreen />
+    // <PostsScreen />
   );
 }
