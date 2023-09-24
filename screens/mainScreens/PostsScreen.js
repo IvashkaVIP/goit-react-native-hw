777f6f7aaa5dfe2b53380665f6e5777c218939e1
IvashkaVIP <ivashka.co@gmail.com>
2023-09-25 00:31:01 +0300

    fix bug of render

--- screens/mainScreens/PostsScreen.js
+++ screens/mainScreens/PostsScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Ionicons, AntDesign, Feather } from "@expo/vector-icons";
@@ -6,22 +6,24 @@ import { Ionicons, AntDesign, Feather } from "@expo/vector-icons";
 const PostsScreen = () => {
   const navigation = useNavigation();
 
-  navigation.setOptions({
+  useEffect(()=>{navigation.setOptions({
     headerTitle: "Публікації",
     headerTintColor: "#212121",
     headerTitleStyle: { fontSize: 17, fontFamily: "Roboto-Medium" },
     headerTitleAlign: "center",
-      headerRight: () => (
-          <Ionicons
-            onPress={() => { navigation.navigate("Login"); }}
-
-        style={{marginRight: 10}}
+    headerRight: () => (
+      <Ionicons
+        onPress={() => {
+          navigation.navigate("Login");
+        }}
+        style={{ marginRight: 10 }}
         name="exit-outline"
         size={24}
         color="#BDBDBD"
       />
     ),
-  });
+  });},[])
+  
 
   return (
     <View style={styles.container}>
