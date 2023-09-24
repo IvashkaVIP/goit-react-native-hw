bd279eb5cd6a4e05de53ea7f926937728ba8cf7a
IvashkaVIP <ivashka.co@gmail.com>
2023-09-24 15:17:58 +0300

    add creatBottomTabNavigator

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+
 import { useState } from "react";
 import { useFonts } from "expo-font";
 import { StyleSheet } from "react-native";
@@ -10,7 +12,8 @@ import RegistrationScreen from "./screens/auth/RegistrationScreen";
 import CreatePostsScreen from "./screens/CreatePostsScreen";
 import PostsScreen from "./screens/PostsScreen";
 
-const MainStack = createStackNavigator();
+const AuthStack = createStackNavigator();
+const MainTab = createBottomTabNavigator();
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,24 +26,24 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <MainStack.Navigator initialRouteName="Registration">
-        <MainStack.Screen
+      <AuthStack.Navigator initialRouteName="Registration">
+        <AuthStack.Screen
           name="Registration"
           component={RegistrationScreen}
           options={{
             headerShown: false,
           }}
         />
-        <MainStack.Screen
+        <AuthStack.Screen
           name="Login"
           component={LoginScreen}
           options={{
             headerShown: false,
           }}
         />
-        <MainStack.Screen name="Post" component={PostsScreen} />
-        <MainStack.Screen name="CreatePosts" component={CreatePostsScreen} />
-      </MainStack.Navigator>
+        <AuthStack.Screen name="Post" component={PostsScreen} />
+        <AuthStack.Screen name="CreatePosts" component={CreatePostsScreen} />
+      </AuthStack.Navigator>
 
       {/* <LoginScreen />
       {/* // <RegistrationScreen />
