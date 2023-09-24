bc58b0f64b145e8788bee57b4800a5350bb24808
IvashkaVIP <ivashka.co@gmail.com>
2023-09-24 17:37:55 +0300

    refactoring

--- App.js
+++ App.js
@@ -1,59 +1,19 @@
 import React from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { useState } from "react";
+
 import { useFonts } from "expo-font";
 import { StyleSheet } from "react-native";
-import LoginScreen from "./screens/auth/LoginScreen";
-import RegistrationScreen from "./screens/auth/RegistrationScreen";
-import CreatePostsScreen from "./screens/CreatePostsScreen";
-import PostsScreen from "./screens/mainScreens/PostsScreen";
-import ProfileScreen from "./screens/mainScreens/ProfileScreen";
 
-const AuthStack = createStackNavigator();
-const MainTab = createBottomTabNavigator();
-
-const useRoute = (isAuth) => {
-  if (!isAuth) {
-    return (
-      <AuthStack.Navigator initialRouteName="Registration">
-        <AuthStack.Screen
-          name="Registration"
-          component={RegistrationScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <AuthStack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <AuthStack.Screen name="Post" component={PostsScreen} />
-        <AuthStack.Screen name="CreatePosts" component={CreatePostsScreen} />
-      </AuthStack.Navigator>
-    );
-  }
-  return (
-    <MainTab.Navigator>
-      <MainTab.Screen name="Posts" component={PostsScreen} />
-      <MainTab.Screen name="CreatePosts" component={CreatePostsScreen} />
-      <MainTab.Screen name="Profile" component={ProfileScreen} />
-    </MainTab.Navigator>
-  );
-};
+import { useRoute } from "./router";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
     "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
   });
-  const routing = useRoute({});
+  const routing = useRoute(null);
   if (!fontsLoaded) {
     return null;
   }
