249a3282358921a6282d6d74778e3ff8b5bef7d1
IvashkaVIP <ivashka.co@gmail.com>
2023-09-29 23:37:07 +0300

    add CreatPost

--- screens/mainScreens/CreatePostsScreen.js
+++ screens/mainScreens/CreatePostsScreen.js
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { TouchableOpacity, StyleSheet, Text, View } from "react-native";
-import { Ionicons, AntDesign, Feather } from "@expo/vector-icons";
-
+import { FontAwesome, Ionicons, AntDesign, Feather } from "@expo/vector-icons";
 
 export const CreatePostsScreen = () => {
   const navigation = useNavigation();
@@ -14,29 +13,69 @@ export const CreatePostsScreen = () => {
       headerTintColor: "#212121",
       headerTitleStyle: { fontSize: 17, fontFamily: "Roboto-Medium" },
       headerTitleAlign: "center",
+      headerStyle: {
+        borderBottomWidth: 1,
+        borderBottomColor: "gray",
+      },
+
       headerLeft: (focused, size, color) => (
-          <Feather
-            style={{ marginLeft: 16, }}
-            onPress={() => navigation.navigate("Posts")}
-            name="arrow-left"
-            size={24}
-            color="rgba(33, 33, 33, 0.8)"
-          />       
+        <Feather
+          style={{ marginLeft: 16 }}
+          onPress={() => navigation.navigate("Posts")}
+          name="arrow-left"
+          size={24}
+          color="rgba(33, 33, 33, 0.8)"
+        />
       ),
     });
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>CreatePostsScreen</Text>
+      <View style={styles.contentBlock}>
+        <View style={styles.cameraWrap}>
+          <FontAwesome name="camera" size={24} color="#BDBDBD" />
+        </View>
+      </View>
+      <View style={styles.textWrap}>
+        <Text style={styles.textFoto}>Завантажте фото</Text>
+      </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    backgroundColor: "white",
+    paddingHorizontal: 16,
+    paddingTop: 32,
+  },
+  contentBlock: {
+    height: 267,
+    width: "100%",
+    backgroundColor: "red",
+    borderRadius: 8,
     justifyContent: "center",
     alignItems: "center",
+    marginBottom: 8
+  },
+  cameraWrap: {
+    borderRadius: 50,
+    width: 60,
+    height: 60,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textWrap: {
+    width: "100%",
+    justifyContent: "flex-start"
+  },
+  textFoto: {
+    fontSize: 16,
+    color: "#BDBDBD",
   },
 });
 
--- screens/mainScreens/Home.js
+++ screens/mainScreens/Home.js
@@ -9,6 +9,7 @@ import PostsScreen from "./PostsScreen";
 import ProfileScreen from "./ProfileScreen";
 
 import { Ionicons, AntDesign, Feather } from "@expo/vector-icons";
+import { Directions } from "react-native-gesture-handler";
 
 const HomeStack = createStackNavigator();
 const HomeTabs = createBottomTabNavigator();
@@ -18,67 +19,63 @@ export default function Home() {
   const route = useRoute();
   
   return (
-  
-       <HomeTabs.Navigator
-          screenOptions={() => ({
-            tabBarShowLabel: false,
-            tabBarInactiveTintColor: "rgba(33, 33, 33, 0.8)",
-            tabBarActiveTintColor: "rgba(255, 00, 00, 0.8)",
+    <HomeTabs.Navigator
+      screenOptions={() => ({
+        tabBarShowLabel: false,
+        tabBarInactiveTintColor: "rgba(33, 33, 33, 0.8)",
+        tabBarActiveTintColor: "rgba(255, 00, 00, 0.8)",
 
-            tabBarStyle: {
-              paddingTop: 9,
-              paddingHorizontal: 60,
-              height: 83,
-              borderTopColor: "#BDBDBD",
-              borderTopWidth: 1,
-            },
-          })}
-        >
-          <HomeTabs.Screen
-            options={({ route }) => ({
-              tabBarIcon: ({ focused, size, color }) => (
-                <AntDesign
-                  // style={styles.footerIconApp}
-                  name="appstore-o"
-                  size={24}
-                  color="#212121"
-                />
-              ),
-            })}
-            name="Posts"
-            component={PostsScreen}
-          />
+        tabBarStyle: {
+          paddingBottom: 34,
+          paddingHorizontal: 60,
+          height: 83,
+          borderTopColor: "#BDBDBD",
+          borderTopWidth: 1,
+        },
+      })}
+    >
+      <HomeTabs.Screen
+        options={({ route }) => ({
+          tabBarIcon: ({ focused, size, color }) => (
+            <AntDesign
+              // style={styles.footerIconApp}
+              name="appstore-o"
+              size={24}
+              color="#212121"
+            />
+          ),
+        })}
+        name="Posts"
+        component={PostsScreen}
+      />
 
-          <HomeTabs.Screen
-            options={({ route }) => ({
-              tabBarIcon: ({ focused, size, color }) => (
-                <TouchableOpacity
-                  style={styles.btn}
-                  activeOpacity={0.75}
-                  onPress={() => {
-                    
-                    navigation.navigate("CreatePosts");                  
-                    
-                  }}
-                >
-                  <Feather name="plus" size={13} color="#FFFFFF" />
-                </TouchableOpacity>
-              ),
-            })}
-            name="CreatePosts"
-            component={CreatePostsScreen}
-          />
-          <HomeTabs.Screen
-            options={({ route }) => ({
-              tabBarIcon: ({ focused, size, color }) => (
-                <Feather name="user" size={24} color="#212121" />
-              ),
-            })}
-            name="Profile"
-            component={ProfileScreen}
-          />
-        </HomeTabs.Navigator>
-          
+      <HomeTabs.Screen
+        options={({ route }) => ({
+          tabBarIcon: ({ focused, size, color }) => (
+            <TouchableOpacity
+              style={styles.btn}
+              activeOpacity={0.75}
+              onPress={() => {
+                navigation.navigate("CreatePosts");
+              }}
+            >
+              <Feather name="plus" size={13} color="#FFFFFF" />
+            </TouchableOpacity>
+          ),
+        })}
+        name="CreatePosts"
+        component={CreatePostsScreen}
+      />
+      <HomeTabs.Screen
+        options={({ route }) => ({
+          tabBarIcon: ({ focused, size, color }) => (
+            <Feather name="user" size={24} color="#212121" />
+          ),
+        })}
+        name="Profile"
+        component={ProfileScreen}
+      />
+    </HomeTabs.Navigator>
   );
 }
 
